fa8e855986850651381bb7b8b236934ed9e95e1f
EngelberAmaya <engelberamaya@gmail.com>
2020-09-30 11:03:02 -0430

    First commit

---
+++ public/js/socket.nuevo-ticket.js
@@ -0,0 +1,28 @@
+// comando para establecer la conexión
+var socket = io();
+
+var label = $('#lblNuevoTicket');
+
+socket.on('connect', function() {
+	console.log('Conectado al servidor');
+});
+
+socket.on('disconnect',function() {
+	console.log('Desconectado al servidor');
+});
+
+// Escuchar información
+socket.on('estadoActual', function(resp){
+     console.log(resp);
+     label.text(resp.actual);
+});
+
+
+
+$('button').on('click', function(){
+
+	socket.emit('siguienteTicket', null, function(siguienteTicket){
+
+		label.text(siguienteTicket);
+	});
+});
